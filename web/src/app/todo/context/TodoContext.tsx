642f85d464b2ed9b50b2466d491b1ed4738571f2
Yurii Vydai <yuriivydai@gmail.com>
2025-05-22 14:33:38 +0300

    update

--- web/src/app/todo/context/TodoContext.tsx
+++ web/src/app/todo/context/TodoContext.tsx
@@ -1,6 +1,7 @@
 import React, { createContext, useEffect, useState } from 'react'
+import { nanoid } from 'nanoid'
 
-const apiUrl = 'http://localhost:8080/api';
+const apiUrl = 'http://localhost:8080/api/todos';
 
 interface TodoContextProps {
   todos: Todo[]
@@ -13,7 +14,7 @@ interface TodoContextProps {
 export interface Todo {
   id?: string
   text: string
-  status: 'undone' | 'completed'
+  status?: 'undone' | 'completed'
 }
 
 export const TodoContext = createContext<TodoContextProps | undefined>(
@@ -25,7 +26,7 @@ export const TodoProvider = (props: { children: React.ReactNode }) => {
 
   useEffect(() => {
     async function fetchPosts() {
-      const res = await fetch(`${apiUrl}/todos`)
+      const res = await fetch(`${apiUrl}`)
       const data = await res.json()
       console.log('data>>>>', data);
       setTodos(data)
@@ -38,9 +39,11 @@ export const TodoProvider = (props: { children: React.ReactNode }) => {
     const newTodo: Todo = {
       text,
       status: 'undone',
+
+      id: nanoid()
     }
 
-    fetch(`${apiUrl}/todos`, {
+    fetch(`${apiUrl}`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(newTodo)
@@ -54,7 +57,7 @@ export const TodoProvider = (props: { children: React.ReactNode }) => {
 
   // ::: DELETE A TODO :::
   const deleteTodo = (id: string) => {
-    fetch(`${apiUrl}/todos/${id}`, {
+    fetch(`${apiUrl}/${id}`, {
       method: 'DELETE',
       headers: { 'Content-Type': 'application/json' }
     })
@@ -67,14 +70,23 @@ export const TodoProvider = (props: { children: React.ReactNode }) => {
 
   // ::: EDIT A TODO :::
   const editTodo = (id: string, text: string) => {
-    setTodos(prevTodos => {
-      return prevTodos.map(todo => {
-        if (todo.id === id) {
-          return { ...todo, text }
-        }
-        return todo
-      })
+    fetch(`${apiUrl}/${id}`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ id, text })
     })
+      .then((res: any) => {
+        setTodos(prevTodos => {
+          return prevTodos.map(todo => {
+            if (todo.id === id) {
+              return { ...todo, text }
+            }
+            return todo
+          })
+        })
+      }).catch((e: Error) => {
+        console.log(e);
+      });
   }
 
   // ::: UPDATE TODO STATUS :::
