297b2393fa4b2153b1e3ecb48d410f10e5c05470
Yurii Vydai <yuriivydai@gmail.com>
2025-05-26 18:22:55 +0300

    swagger additional endpoints

--- packages/api/dist/config/swagger.config.js
+++ packages/api/dist/config/swagger.config.js
@@ -33,6 +33,16 @@ exports.swaggerConfig = {
                             },
                         },
                     },
+                    '500': {
+                        description: 'Internal server error',
+                        content: {
+                            'application/json': {
+                                schema: {
+                                    $ref: '#/components/schemas/Error',
+                                },
+                            },
+                        },
+                    },
                 },
             },
             post: {
@@ -55,6 +65,222 @@ exports.swaggerConfig = {
                             'application/json': {
                                 schema: {
                                     $ref: '#/components/schemas/Todo',
+                                },
+                            },
+                        },
+                    },
+                    '400': {
+                        description: 'Invalid input',
+                        content: {
+                            'application/json': {
+                                schema: {
+                                    $ref: '#/components/schemas/Error',
+                                },
+                            },
+                        },
+                    },
+                    '500': {
+                        description: 'Internal server error',
+                        content: {
+                            'application/json': {
+                                schema: {
+                                    $ref: '#/components/schemas/Error',
+                                },
+                            },
+                        },
+                    },
+                },
+            },
+            delete: {
+                tags: ['todos'],
+                summary: 'Delete all todos',
+                responses: {
+                    '204': {
+                        description: 'All todos deleted successfully',
+                    },
+                    '500': {
+                        description: 'Internal server error',
+                        content: {
+                            'application/json': {
+                                schema: {
+                                    $ref: '#/components/schemas/Error',
+                                },
+                            },
+                        },
+                    },
+                },
+            },
+        },
+        '/api/todos/published': {
+            get: {
+                tags: ['todos'],
+                summary: 'Get all completed todos',
+                responses: {
+                    '200': {
+                        description: 'List of completed todos',
+                        content: {
+                            'application/json': {
+                                schema: {
+                                    type: 'array',
+                                    items: {
+                                        $ref: '#/components/schemas/Todo',
+                                    },
+                                },
+                            },
+                        },
+                    },
+                    '500': {
+                        description: 'Internal server error',
+                        content: {
+                            'application/json': {
+                                schema: {
+                                    $ref: '#/components/schemas/Error',
+                                },
+                            },
+                        },
+                    },
+                },
+            },
+        },
+        '/api/todos/{id}': {
+            get: {
+                tags: ['todos'],
+                summary: 'Get a todo by ID',
+                parameters: [
+                    {
+                        name: 'id',
+                        in: 'path',
+                        required: true,
+                        description: 'ID of the todo to retrieve',
+                        schema: {
+                            type: 'string',
+                        },
+                    },
+                ],
+                responses: {
+                    '200': {
+                        description: 'Todo found',
+                        content: {
+                            'application/json': {
+                                schema: {
+                                    $ref: '#/components/schemas/Todo',
+                                },
+                            },
+                        },
+                    },
+                    '404': {
+                        description: 'Todo not found',
+                        content: {
+                            'application/json': {
+                                schema: {
+                                    $ref: '#/components/schemas/Error',
+                                },
+                            },
+                        },
+                    },
+                    '500': {
+                        description: 'Internal server error',
+                        content: {
+                            'application/json': {
+                                schema: {
+                                    $ref: '#/components/schemas/Error',
+                                },
+                            },
+                        },
+                    },
+                },
+            },
+            put: {
+                tags: ['todos'],
+                summary: 'Update a todo',
+                parameters: [
+                    {
+                        name: 'id',
+                        in: 'path',
+                        required: true,
+                        description: 'ID of the todo to update',
+                        schema: {
+                            type: 'string',
+                        },
+                    },
+                ],
+                requestBody: {
+                    required: true,
+                    content: {
+                        'application/json': {
+                            schema: {
+                                $ref: '#/components/schemas/TodoUpdate',
+                            },
+                        },
+                    },
+                },
+                responses: {
+                    '200': {
+                        description: 'Todo updated successfully',
+                        content: {
+                            'application/json': {
+                                schema: {
+                                    $ref: '#/components/schemas/Todo',
+                                },
+                            },
+                        },
+                    },
+                    '404': {
+                        description: 'Todo not found',
+                        content: {
+                            'application/json': {
+                                schema: {
+                                    $ref: '#/components/schemas/Error',
+                                },
+                            },
+                        },
+                    },
+                    '500': {
+                        description: 'Internal server error',
+                        content: {
+                            'application/json': {
+                                schema: {
+                                    $ref: '#/components/schemas/Error',
+                                },
+                            },
+                        },
+                    },
+                },
+            },
+            delete: {
+                tags: ['todos'],
+                summary: 'Delete a todo',
+                parameters: [
+                    {
+                        name: 'id',
+                        in: 'path',
+                        required: true,
+                        description: 'ID of the todo to delete',
+                        schema: {
+                            type: 'string',
+                        },
+                    },
+                ],
+                responses: {
+                    '204': {
+                        description: 'Todo deleted successfully',
+                    },
+                    '404': {
+                        description: 'Todo not found',
+                        content: {
+                            'application/json': {
+                                schema: {
+                                    $ref: '#/components/schemas/Error',
+                                },
+                            },
+                        },
+                    },
+                    '500': {
+                        description: 'Internal server error',
+                        content: {
+                            'application/json': {
+                                schema: {
+                                    $ref: '#/components/schemas/Error',
                                 },
                             },
                         },
@@ -100,6 +326,32 @@ exports.swaggerConfig = {
                         description: 'The status of the todo',
                         enum: ['done', 'undone'],
                         example: 'undone',
+                    },
+                },
+            },
+            TodoUpdate: {
+                type: 'object',
+                properties: {
+                    text: {
+                        type: 'string',
+                        description: 'The text content of the todo',
+                        example: 'Buy groceries',
+                    },
+                    status: {
+                        type: 'string',
+                        description: 'The status of the todo',
+                        enum: ['done', 'undone'],
+                        example: 'done',
+                    },
+                },
+            },
+            Error: {
+                type: 'object',
+                properties: {
+                    message: {
+                        type: 'string',
+                        description: 'Error message',
+                        example: 'An error occurred',
                     },
                 },
             },
--- packages/api/src/config/swagger.config.ts
+++ packages/api/src/config/swagger.config.ts
@@ -30,6 +30,16 @@ export const swaggerConfig = {
               },
             },
           },
+          '500': {
+            description: 'Internal server error',
+            content: {
+              'application/json': {
+                schema: {
+                  $ref: '#/components/schemas/Error',
+                },
+              },
+            },
+          },
         },
       },
       post: {
@@ -52,6 +62,222 @@ export const swaggerConfig = {
               'application/json': {
                 schema: {
                   $ref: '#/components/schemas/Todo',
+                },
+              },
+            },
+          },
+          '400': {
+            description: 'Invalid input',
+            content: {
+              'application/json': {
+                schema: {
+                  $ref: '#/components/schemas/Error',
+                },
+              },
+            },
+          },
+          '500': {
+            description: 'Internal server error',
+            content: {
+              'application/json': {
+                schema: {
+                  $ref: '#/components/schemas/Error',
+                },
+              },
+            },
+          },
+        },
+      },
+      delete: {
+        tags: ['todos'],
+        summary: 'Delete all todos',
+        responses: {
+          '204': {
+            description: 'All todos deleted successfully',
+          },
+          '500': {
+            description: 'Internal server error',
+            content: {
+              'application/json': {
+                schema: {
+                  $ref: '#/components/schemas/Error',
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+    '/api/todos/published': {
+      get: {
+        tags: ['todos'],
+        summary: 'Get all completed todos',
+        responses: {
+          '200': {
+            description: 'List of completed todos',
+            content: {
+              'application/json': {
+                schema: {
+                  type: 'array',
+                  items: {
+                    $ref: '#/components/schemas/Todo',
+                  },
+                },
+              },
+            },
+          },
+          '500': {
+            description: 'Internal server error',
+            content: {
+              'application/json': {
+                schema: {
+                  $ref: '#/components/schemas/Error',
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+    '/api/todos/{id}': {
+      get: {
+        tags: ['todos'],
+        summary: 'Get a todo by ID',
+        parameters: [
+          {
+            name: 'id',
+            in: 'path',
+            required: true,
+            description: 'ID of the todo to retrieve',
+            schema: {
+              type: 'string',
+            },
+          },
+        ],
+        responses: {
+          '200': {
+            description: 'Todo found',
+            content: {
+              'application/json': {
+                schema: {
+                  $ref: '#/components/schemas/Todo',
+                },
+              },
+            },
+          },
+          '404': {
+            description: 'Todo not found',
+            content: {
+              'application/json': {
+                schema: {
+                  $ref: '#/components/schemas/Error',
+                },
+              },
+            },
+          },
+          '500': {
+            description: 'Internal server error',
+            content: {
+              'application/json': {
+                schema: {
+                  $ref: '#/components/schemas/Error',
+                },
+              },
+            },
+          },
+        },
+      },
+      put: {
+        tags: ['todos'],
+        summary: 'Update a todo',
+        parameters: [
+          {
+            name: 'id',
+            in: 'path',
+            required: true,
+            description: 'ID of the todo to update',
+            schema: {
+              type: 'string',
+            },
+          },
+        ],
+        requestBody: {
+          required: true,
+          content: {
+            'application/json': {
+              schema: {
+                $ref: '#/components/schemas/TodoUpdate',
+              },
+            },
+          },
+        },
+        responses: {
+          '200': {
+            description: 'Todo updated successfully',
+            content: {
+              'application/json': {
+                schema: {
+                  $ref: '#/components/schemas/Todo',
+                },
+              },
+            },
+          },
+          '404': {
+            description: 'Todo not found',
+            content: {
+              'application/json': {
+                schema: {
+                  $ref: '#/components/schemas/Error',
+                },
+              },
+            },
+          },
+          '500': {
+            description: 'Internal server error',
+            content: {
+              'application/json': {
+                schema: {
+                  $ref: '#/components/schemas/Error',
+                },
+              },
+            },
+          },
+        },
+      },
+      delete: {
+        tags: ['todos'],
+        summary: 'Delete a todo',
+        parameters: [
+          {
+            name: 'id',
+            in: 'path',
+            required: true,
+            description: 'ID of the todo to delete',
+            schema: {
+              type: 'string',
+            },
+          },
+        ],
+        responses: {
+          '204': {
+            description: 'Todo deleted successfully',
+          },
+          '404': {
+            description: 'Todo not found',
+            content: {
+              'application/json': {
+                schema: {
+                  $ref: '#/components/schemas/Error',
+                },
+              },
+            },
+          },
+          '500': {
+            description: 'Internal server error',
+            content: {
+              'application/json': {
+                schema: {
+                  $ref: '#/components/schemas/Error',
                 },
               },
             },
@@ -97,6 +323,32 @@ export const swaggerConfig = {
             description: 'The status of the todo',
             enum: ['done', 'undone'],
             example: 'undone',
+          },
+        },
+      },
+      TodoUpdate: {
+        type: 'object',
+        properties: {
+          text: {
+            type: 'string',
+            description: 'The text content of the todo',
+            example: 'Buy groceries',
+          },
+          status: {
+            type: 'string',
+            description: 'The status of the todo',
+            enum: ['done', 'undone'],
+            example: 'done',
+          },
+        },
+      },
+      Error: {
+        type: 'object',
+        properties: {
+          message: {
+            type: 'string',
+            description: 'Error message',
+            example: 'An error occurred',
           },
         },
       },
